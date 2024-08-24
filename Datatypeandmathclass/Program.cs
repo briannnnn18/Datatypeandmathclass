7f6038f5599c9ee9129cbf8af2439958f5eee027
ACER <ACER@DESKTOP-N870L3J>
2024-08-24 14:08:25 +0800

    Add project files.

---
+++ Datatypeandmathclass/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            //question grades
+            double[] grades = new double[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter your grade in subject {i + 1}: ");
+                grades[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            // Computation
+            double sum = 0;
+            foreach (double grade in grades)
+            {
+                sum += grade;
+            }
+            double average = sum / grades.Length;
+
+            // rounding it off
+            double roundedAverage = Math.Round(average, 2);
+
+            // resualt
+            Console.WriteLine($"Average of the grades: {average:F2}");
+            Console.WriteLine($"Rounded average: {roundedAverage:F2}");
+            
+            Console.WriteLine("Press any key to exit..... ");
+            Console.ReadKey();
+        }
+    }
+}
